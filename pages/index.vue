5572796594a79f31731b43e47b5bba1756281f39
Alain Hélaïli <helaili@github.com>
2021-01-06 22:12:23 +0100

    cards are all same size

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-row justify="center" align="center">
+  <v-row>
     <v-col v-for="repo of repositories.slice(pageStart, pageStart+pageSize)" v-bind:key="repo.name" cols="6">
-      <v-card height="200">  
-        <v-card-title class="headline">
+      <v-card>   
+        <v-card-title>
           {{repo.name}}
         </v-card-title>
         <v-card-subtitle>{{repo.full_name}}</v-card-subtitle>
         <v-card-text>
           <p class="description">{{repo.description || '&nbsp;'}}</p>
-          <v-chip-group>
-            <v-chip v-for="topic of repo.topics" v-bind:key="topic">{{topic}}</v-chip>
+        </v-card-text>
+        <v-card-text>
+          <v-chip-group column>
+            <v-chip small v-for="topic of repo.topics" v-bind:key="topic">{{topic}}</v-chip>
           </v-chip-group>
         </v-card-text>
-        <v-card-actions class="repo_stats">
+        <v-card-actions v-bind:class="{ 'no-topic': repo.topics.length == 0 }">
           <v-btn>
             <v-icon left>mdi-eye-outline</v-icon>
             {{repo.watchers_count}}
@@ -25,8 +27,6 @@
             <v-icon left>mdi-source-fork</v-icon>
             {{repo.forks_count}}
           </v-btn>
-          <v-spacer />
-          
         </v-card-actions>
       </v-card>
     </v-col>
@@ -56,8 +56,13 @@ export default {
   text-overflow: ellipsis;
 }
 
-.repo_stats {
-  position: absolute; 
-  bottom: 0px; 
+.v-card {
+  height: 100%;
 }
+
+.no-topic {
+  bottom: 0;
+  position: absolute;
+}
+
 </style>

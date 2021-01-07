ec9fb77309270331a6812c9dbe1b284f37e7cd79
Alain Hélaïli <helaili@github.com>
2021-01-07 12:26:31 +0100

    added pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,44 @@
 <template>
-  <v-row>
-    <v-col v-for="repo of repositories.slice(pageStart, pageStart+pageSize)" v-bind:key="repo.name" cols="6">
-      <v-card>   
-        <v-card-title>
-          {{repo.name}}
-        </v-card-title>
-        <v-card-subtitle>{{repo.full_name}}</v-card-subtitle>
-        <v-card-text>
-          <p class="description">{{repo.description || '&nbsp;'}}</p>
-        </v-card-text>
-        <v-card-text>
-          <v-chip-group column>
-            <v-chip small v-for="topic of repo.topics" v-bind:key="topic">{{topic}}</v-chip>
-          </v-chip-group>
-        </v-card-text>
-        <v-card-actions v-bind:class="{ 'no-topic': repo.topics.length == 0 }">
-          <v-btn>
-            <v-icon left>mdi-eye-outline</v-icon>
-            {{repo.watchers_count}}
-          </v-btn>
-          <v-btn>
-            <v-icon left>mdi-star-outline</v-icon>
-            {{repo.stargazers_count}}
-          </v-btn>
-          <v-btn>
-            <v-icon left>mdi-source-fork</v-icon>
-            {{repo.forks_count}}
-          </v-btn>
-          <v-spacer />
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row>
+      <v-col v-for="repo of repositories.slice(pageStart, pageStart+pageSize)" v-bind:key="repo.name" cols="6">
+        <v-card>   
+          <v-card-title>
+            {{repo.name}}
+          </v-card-title>
+          <v-card-subtitle>{{repo.full_name}}</v-card-subtitle>
+          <v-card-text>
+            <p class="description">{{repo.description || '&nbsp;'}}</p>
+          </v-card-text>
+          <v-card-text>
+            <v-chip-group column>
+              <v-chip small v-for="topic of repo.topics" v-bind:key="topic">{{topic}}</v-chip>
+            </v-chip-group>
+          </v-card-text>
+          <v-card-actions v-bind:class="{ 'no-topic': repo.topics.length == 0 }">
+            <v-btn>
+              <v-icon left>mdi-eye-outline</v-icon>
+              {{repo.watchers_count}}
+            </v-btn>
+            <v-btn>
+              <v-icon left>mdi-star-outline</v-icon>
+              {{repo.stargazers_count}}
+            </v-btn>
+            <v-btn>
+              <v-icon left>mdi-source-fork</v-icon>
+              {{repo.forks_count}}
+            </v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row align="center">
+      <v-col>
+        <v-pagination v-model="page" :length="totalPages" @input="goToPage"></v-pagination>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -41,8 +48,20 @@ export default {
   data() {
     return{
       repositories: repoFile,
+      page: 1,
       pageStart: 0,
-      pageSize: 30
+      pageSize: 40
+    }
+  }, 
+  computed: {
+    totalPages: function() {
+      return Math.floor(this.repositories.length / this.pageSize) + 1
+    }
+  },
+  methods: {
+    goToPage: function(input) {
+      console.log(input)
+      this.pageStart = (input-1) * this.pageSize
     }
   }
 }

bde2a313114e899423ba29db6df466a6634d5f38
Alain Hélaïli <helaili@github.com>
2021-01-07 22:09:09 +0100

    adding a router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
+  router: {
+    base: '/enterprise-showcase/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - enterprise-showcase',

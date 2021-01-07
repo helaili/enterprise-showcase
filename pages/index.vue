363a12ae2dd3ea4b431c4ad9db648dc61a080476
Alain Hélaïli <helaili@github.com>
2021-01-07 12:51:56 +0100

    Make linter happy

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,44 @@
 <template>
   <v-container>
     <v-row>
-      <v-col v-for="repo of repositories.slice(pageStart, pageStart+pageSize)" v-bind:key="repo.full_name" cols="6">
-        <v-card>   
+      <v-col v-for="repo of repositories.slice(pageStart, pageStart+pageSize)" :key="repo.full_name" cols="6">
+        <v-card>
           <v-card-title>
-            <a v-bind:href="repo.html_url">{{repo.name}}</a>
+            <a :href="repo.html_url">{{ repo.name }}</a>
           </v-card-title>
           <v-card-subtitle>
-            <a v-bind:href="repo.html_url">{{repo.full_name}}</a>
+            <a :href="repo.html_url">{{ repo.full_name }}</a>
           </v-card-subtitle>
           <v-card-text>
-            <p class="description">{{repo.description || '&nbsp;'}}</p>
+            <p class="description">
+              {{ repo.description || '&nbsp;' }}
+            </p>
           </v-card-text>
           <v-card-text>
             <v-chip-group column>
-              <v-chip small v-for="topic of repo.topics" v-bind:key="topic">{{topic}}</v-chip>
+              <v-chip v-for="topic of repo.topics" :key="topic" small>
+                {{ topic }}
+              </v-chip>
             </v-chip-group>
           </v-card-text>
-          <v-card-actions v-bind:class="{ 'no-topic': repo.topics.length == 0 }">
+          <v-card-actions :class="{ 'no-topic': repo.topics.length == 0 }">
             <v-btn>
-              <v-icon left>mdi-eye-outline</v-icon>
-              {{repo.watchers_count}}
+              <v-icon left>
+                mdi-eye-outline
+              </v-icon>
+              {{ repo.watchers_count }}
             </v-btn>
             <v-btn>
-              <v-icon left>mdi-star-outline</v-icon>
-              {{repo.stargazers_count}}
+              <v-icon left>
+                mdi-star-outline
+              </v-icon>
+              {{ repo.stargazers_count }}
             </v-btn>
             <v-btn>
-              <v-icon left>mdi-source-fork</v-icon>
-              {{repo.forks_count}}
+              <v-icon left>
+                mdi-source-fork
+              </v-icon>
+              {{ repo.forks_count }}
             </v-btn>
             <v-spacer />
           </v-card-actions>
@@ -37,7 +47,7 @@
     </v-row>
     <v-row align="center">
       <v-col>
-        <v-pagination v-model="page" :length="totalPages" @input="goToPage"></v-pagination>
+        <v-pagination v-model="page" :length="totalPages" @input="goToPage" />
       </v-col>
     </v-row>
   </v-container>
@@ -47,22 +57,22 @@
 import repoFile from '~/repositories.json'
 
 export default {
-  data() {
-    return{
+  data () {
+    return {
       repositories: repoFile,
       page: 1,
       pageStart: 0,
       pageSize: 40
     }
-  }, 
+  },
   computed: {
-    totalPages: function() {
+    totalPages: () => {
       return Math.floor(this.repositories.length / this.pageSize) + 1
     }
   },
   methods: {
-    goToPage: function(input) {
-      this.pageStart = (input-1) * this.pageSize
+    goToPage: (input) => {
+      this.pageStart = (input - 1) * this.pageSize
     }
   }
 }
